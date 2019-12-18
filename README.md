0282e9f2d803428f172e2d7cb0f1f92def892308
whitestar.lau <whitestar.lau@gmail.com>
2019-12-18 20:56:32 +0800

    init commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+## 这个项目是什么
+
+ 这是一个Android图片编辑库。主体代码来自于
+ -  https://github.com/burhanrashid52/PhotoEditor 
+ -  https://github.com/jfeinstein10/DominantColors
+ 
+ 第一个项目是编辑的主体，第二个项目用于涂抹方法，即使用kmeans聚合算法取出涂抹时需要的颜色。因为第二个项目年久失修，主要工作是将项目的jni迁移到cmake，并完成了之前未完成的聚合出的颜色百分比统计功能。
---
+++ app/src/main/java/com/white/retouchdemo/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.white.retouchdemo
+
+//import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.white.component_retouch.PicEditor.OnPhotoEditorListener
+import com.white.component_retouch.PicEditor.PhotoEditor
+import com.white.component_retouch.PicEditor.ViewType
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import java.lang.ref.WeakReference
+
+class MainActivity : AppCompatActivity() {
+    private val PICK_REQUEST = 251
+    private var mPhotoEditor: PhotoEditor? = null
+
+    var defaultSize = 50f
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        openGallery()
+    }
+
+    fun openGallery() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(
+            Intent.createChooser(intent, "Select Picture"),
+            PICK_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == RESULT_OK) {
+            when (requestCode) {
+
+                PICK_REQUEST -> try {
+                    val uri = data?.data
+
+                    val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+                    photoEditorView.source.setImageBitmap(bitmap)
+
+                    initPhotoEditor()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    fun initPhotoEditor(){
+        mPhotoEditor = PhotoEditor.Builder(this, photoEditorView)
+            .setPinchTextScalable(true) // set flag to make text scalable when pinch
+            .build()
+        mPhotoEditor?.setOnPhotoEditorListener(WeakListener(this))
+
+        mPhotoEditor?.setBrushDrawingMode(true)
+        mPhotoEditor?.setReTouchModeMode(true)
+        mPhotoEditor?.brushSize = defaultSize
+    }
+
+    private class WeakListener(activity: MainActivity) : OnPhotoEditorListener {
+        private val mWfPresenter: WeakReference<MainActivity>?
+
+        init {
+            this.mWfPresenter = WeakReference(activity)
+        }
+
+        override fun onEditTextChangeListener(rootView: View?, text: String?, colorCode: Int) {
+
+        }
+
+        override fun onAddViewListener(viewType: ViewType?, numberOfAddedViews: Int) {
+
+        }
+
+        override fun onRemoveViewListener(viewType: ViewType?, numberOfAddedViews: Int) {
+
+        }
+
+        override fun onStartViewChangeListener(viewType: ViewType?) {
+
+        }
+
+        override fun onStopViewChangeListener(viewType: ViewType?) {
+
+        }
+    }
+}
